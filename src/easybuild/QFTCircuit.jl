f8b22ab1f56782c70e4452867ae934fabae646d0
Roger-luo <hiroger@qq.com>
2019-12-07 23:07:01 -0500

    transform -> hcphases

--- src/easybuild/QFTCircuit.jl
+++ src/easybuild/QFTCircuit.jl
@@ -6,11 +6,27 @@ export qft_circuit
 Control phase gate.
 """
 cphase(i::Int, j::Int, k::Int) = control(i, j=>shift(2π/(1<<k)))
-transform(n::Int, i::Int) = chain(n, i==j ? put(i=>H) : cphase(j, i, j-i+1) for j = i:n)
+hcphases(n::Int, i::Int) = chain(n, i==j ? put(i=>H) : cphase(j, i, j-i+1) for j = i:n)
 
 """
     qft_circuit(n)
 
 Create a Quantum Fourer Transform circuit. See also [`QFT`](@ref).
 """
-qft_circuit(n::Int) = chain(n, transform(n, i) for i = 1:n)
+qft_circuit(n::Int) = chain(n, hcphases(n, i) for i = 1:n)
+
+qft(l, n) = l == n ? H : chain(
+    chain(n, l==j ? put(l=>H) : cphase(j, l, j-l+1) for j = l:n),
+    qft(l-1, n)
+)
+
+function qft(l, n)
+    if l == n
+        return put(n, l=>H)
+    else
+        return chain(
+            chain(n, l==j ? put(l=>H) : cphase(j, l, j-l+1) for j = l:n),
+            qft(l+1, n)
+        )
+    end
+end
